cd86453924db21c8ea9d004f16fded59574ddf62
Michael Lazarski <micha@quantilopes-MacBook-Pro.local>
2016-11-02 10:09:37 +0100

    init commit

---
+++ packages/simplePackage/package.js
@@ -0,0 +1,43 @@
+Package.describe({
+    name: 'simple:package',
+    version: '0.0.2',
+});
+
+Package.onUse(function (api) {
+    api.versionsFrom('1.4.2');
+    api.use('ecmascript');
+
+    api.use([
+        'random',
+        'underscore'
+    ], [ 'client', 'server' ]);
+
+    api.addFiles('simplePackage.js');
+
+    api.use([
+        'jquery'
+    ], 'client');
+});
+
+Npm.depends({
+  'cheerio': '0.22.0'
+})
+
+Package.onTest(function (api) {
+    api.versionsFrom('1.4.2');
+    api.use('ecmascript');
+
+    api.use([
+        'practicalmeteor:mocha',
+        'practicalmeteor:chai',
+        'practicalmeteor:sinon'
+    ]);
+
+    api.addFiles('simplePackage.js');
+
+    api.use([
+        'mongo',
+        'random',
+        'underscore'
+    ]);
+});
